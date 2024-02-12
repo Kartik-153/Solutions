41c8dd45557e7ee93c5932a3761b38192f9a1630
Kartik-153 <106980372+Kartik-153@users.noreply.github.com>
2024-02-12 20:09:47 +0530

    Create PlusOne.cpp

---
+++ PlusOne.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+    
+    for (int i = n - 1; i >= 0; i--) {
+        digits[i]++;
+        
+        if (digits[i] < 10) {
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+    
+    digits.insert(digits.begin(), 1);
+    
+    return digits;
+    }
+};
