ae03c0a4d40e1b3c516fb20fa568243e8cc45502
Kartik-153 <106980372+Kartik-153@users.noreply.github.com>
2024-02-12 20:27:07 +0530

    Create TrailingZeroesInAnInteger.cpp

---
+++ TrailingZeroesInAnInteger.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if(n<0) {
+            return -1;
+        }
+
+        int count = 0;
+        for(int i = 5; n/i >= 1; i *= 5) {
+            count = count + n/i;
+        }
+        return count;
+    }
+};
