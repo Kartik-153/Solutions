dc23e60511b9fd955410005e5bd6c62d2f28f19e
Kartik-153 <106980372+Kartik-153@users.noreply.github.com>
2024-05-07 20:31:50 +0530

    Create FindMinmumInRotatedArray.cpp

---
+++ FindMinmumInRotatedArray.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans = INT_MAX;
+        while(low<=high) {
+            int mid = (low+high)/2;
+            if(nums[low] <= nums[mid]) {
+                ans = min(ans, nums[low]);
+                low = mid+1;
+            }
+            else{
+                ans = min(nums[mid], ans);
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
