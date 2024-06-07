b02f8d21b4315fd1b904ef1e138d4becd878d2db
Kartik-153 <106980372+Kartik-153@users.noreply.github.com>
2024-06-07 21:10:00 +0530

    Create FindPeakEle,ent.cpp

---
+++ FindPeakEle,ent.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int n = nums.size();
+        while(low <= high) {
+            int mid = (low + high) / 2;
+
+            if((mid == 0 || nums[mid-1] <= nums[mid]) && 
+                (mid == n - 1 || nums[mid+1] <= nums[mid]))
+                return mid;
+            if(mid > 0 && nums[mid-1] >= nums[mid])
+                high = mid - 1;
+            else 
+                low = mid + 1;
+        }
+        return -1;
+    }
+};
