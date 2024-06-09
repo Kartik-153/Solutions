c3818f78a1c4904673eb226ccce3784f8f6ab279
Kartik-153 <106980372+Kartik-153@users.noreply.github.com>
2024-06-09 22:48:06 +0530

    Create SubarrayWithSumDivisibleByK.cpp

---
+++ SubarrayWithSumDivisibleByK.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int cnt = 0;
+        int prefixSum = 0;
+        unordered_map<int, int> prefixMap;
+        prefixMap[0] = 1;
+        for (int num : nums) {
+            prefixSum += num;
+            int mod = prefixSum % k;
+            if (mod < 0) {
+                mod += k;
+            }
+            if (prefixMap.find(mod) != prefixMap.end()) {
+                cnt += prefixMap[mod];
+                prefixMap[mod] += 1;
+            } else {
+                prefixMap[mod] = 1;
+            }
+        }
+        return cnt;
+    }
+};
