e410494b72e19275a211d7b905d4326b47161f37
Kartik-153 <106980372+Kartik-153@users.noreply.github.com>
2024-08-01 15:17:39 +0530

    Create SumRoottoLeafNumbers.cpp

---
+++ SumRoottoLeafNumbers.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);
+    }
+
+private:
+    int dfs(TreeNode* node, int currSum) {
+        if(!node) {
+            return 0;
+        }
+
+        currSum = currSum * 10 + node->val;
+
+        if(!node->left && !node->right) {
+            return currSum;
+        }
+
+        return dfs(node->left, currSum) + dfs(node->right, currSum);
+    }
+};
