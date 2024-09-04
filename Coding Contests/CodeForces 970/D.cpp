d422aeac3f06c8485cf96c7be8b0772aebcb6dc1
kartik <kartikswaroop.007@gmail.com>
2024-09-04 22:48:14 +0530

    modified:   Coding Contests/CodeForces 970/D.cpp 	deleted:    VS Code/CppProperties.json

--- Coding Contests/CodeForces 970/D.cpp
+++ Coding Contests/CodeForces 970/D.cpp
@@ -27,12 +27,13 @@ void solve() {
         cin >> p[i];
     }
     
-    
+
 }
  
 int32_t main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
     int t;
     cin >> t;
         while(t--) {
