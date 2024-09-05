e9495af790f7423340d8a5d8f47c107572303c20
kartik <kartikswaroop.007@gmail.com>
2024-09-05 09:42:20 +0530

    modified:   Coding Contests/CodeForces 970/D.cpp

--- Coding Contests/CodeForces 970/D.cpp
+++ Coding Contests/CodeForces 970/D.cpp
@@ -1,6 +1,23 @@
+/**
+*  जय सिया राम
+*/
+ 
 #include <bits/stdc++.h>
-
 using namespace std;
+  
+#define int           long long int
+#define endl          "\n" 
+#define pb            push_back
+#define F             first
+#define S             second
+#define rep(i,a,n)    for(int i = a; i < n; i++)
+#define all(p)        p.begin(), p.end()
+#define double        long double
+#define que_max       priority_queue <int>
+#define print(a)      for(auto x : a) cout << x << " "; cout << endl;
+#define print1(a)     for(auto x : a) cout << x.F << " " << x.S << endl;
+#define mod           1000000007
+ 
 
 const int N = 200010;
 int n, a[N], ans[N], cnt;
@@ -39,11 +56,14 @@ void solve() {
     printf("\n");
 }
 
-int main() {
+ 
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int t;
     cin >> t;
-    while (t--) {
-        solve();
+        while(t--) {
+           solve();
     }
     return 0;
 }
