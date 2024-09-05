9879f5109be977e50fb96f52184d5f8d6c31c1d6
kartik <kartikswaroop.007@gmail.com>
2024-09-05 09:32:27 +0530

    modified:   Coding Contests/CodeForces 970/D.cpp

--- Coding Contests/CodeForces 970/D.cpp
+++ Coding Contests/CodeForces 970/D.cpp
@@ -1,59 +1,49 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
-int main() {
-    int t, n, i, j, r, count, tb, no;
-    cin>>t;
-    while(t--)
-    {
-        cin>>n;
-        string s;
-        cin>>s;
-        r = static_cast<int>(sqrt(n));
-       // cout<<r<<endl;
-       count=0, no=0, tb=0;
-        if(r*r!=n)
-            cout<<"No"<<endl;
-        else
-        {
-            for(i=0; i<n; i++)
-            {
-                if(i<r || i>= r*(r-1))
-                {
-                    if(s[i]!='1')
-                    {
-                        no++;
-                        break;
-                    }
-                }
-                else
-                {
-                    if(i%r==0 || i%r==r-1)
-                    {
-                        if(s[i]!='1')
-                        {
-                            no++;
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        if(s[i]!='0')
-                        {
-                            no++;
-                            break;
-                        }
-                    }
-                }
-            }
-            if(no>0)
-                cout<<"No"<<endl;
-            else
-                cout<<"Yes"<<endl;
-            
+
+const int N = 200010;
+int n, a[N], ans[N], cnt;
+bool vis[N];
+string s;
+
+int dfs(int p){
+    if (vis[p - 1]){
+        return cnt;
+    }
+    vis[p - 1] = 1;
+    if(s[p - 1] == '0') cnt++;
+    ans[p - 1] = dfs(a[p - 1]);
+    return cnt;
+}
+
+void solve() {
+    cin >> n;
+    memset(a, 0, sizeof(a));
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    s = "";
+    cin >> s;
+    memset(ans, -1, sizeof(ans));
+    for (int i = 1; i <= n; i++) {
+        if (ans[i - 1] == -1) {
+            cnt = 0;
+            memset(vis, 0, sizeof(vis));
+            ans[i - 1] = dfs(i);
         }
     }
+    for(int i = 0; i < n; i++){
+        printf("%d ",ans[i]);
+    }
+    printf("\n");
+}
 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
