5993969e0c33a290e826fca25c742b93da72afaa
kartik <kartikswaroop.007@gmail.com>
2024-09-14 23:26:33 +0530

    new file:   .gitignore 	new file:   LeetCode/reverseLevelOrder.cpp

---
+++ LeetCode/reverseLevelOrder.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root == nullptr) 
+            return res;
+
+        
+    }
+};
