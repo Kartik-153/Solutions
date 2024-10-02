994bb7250f3771c47469bb575dc47537aaaf45cb
kartik <kartikswaroop.007@gmail.com>
2024-10-02 12:00:14 +0530

    modified:   LeetCode/SingleNumberII.cpp

--- LeetCode/SingleNumberII.cpp
+++ LeetCode/SingleNumberII.cpp
@@ -4,12 +4,11 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int x1 = nums[0];
-        
-        for(int i = 1; i < nums.size(); i++) {
-            x1 ^= nums[i];
+        int ones = 0, twos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
-
-        
+        return ones;
     }
 };
