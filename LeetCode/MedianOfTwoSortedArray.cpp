b0e802942f564cde94df0bd6f4ecc9f2403c36c5
kartik <kartikswaroop.007@gmail.com>
2024-10-12 15:35:26 +0530

    renamed:    LeetCode/FindPeakEle,ent.cpp -> LeetCode/FindPeakElement.cpp 	new file:   LeetCode/MedianOfTwoSortedArray.cpp

---
+++ LeetCode/MedianOfTwoSortedArray.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int p1 = 0, p2 = 0;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = int(nums1.size()), n = int(nums2.size());
+
+        if((m+n) % 2 == 0) {
+            for(int i = 0; i < (m+n)/2 -1; i++) {
+                int _ = getMin(nums1, nums2);
+            }
+            return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
+        } else {
+            for(int i = 0; i < (m + n) / 2; i++) {
+                int _ = getMin(nums1, nums2);
+            }
+            return getMin(nums1, nums2);
+        }
+        return -1;
+    }
+
+private:
+    int getMin(vector<int>& nums1, vector<int>& nums2) {
+        if (p1 < nums1.size() && p2 < nums2.size()) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < nums1.size()) {
+            return nums1[p1++];
+        } else if (p2 < nums2.size()) {
+            return nums2[p2++];
+        }
+        return -1;
+    }
+};
+
+class Solution2 {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.size(), n = nums2.size();
+        int left = 0, right = m;
+
+        while (left <= right) {
+            int partitionA = (left + right) / 2;
+            int partitionB = (m + n + 1) / 2 - partitionA;
+
+            int maxLeftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+            int minRightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+            int maxLeftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+            int minRightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftA, maxLeftB) +
+                            min(minRightA, minRightB)) /
+                           2.0;
+                } else {
+                    return max(maxLeftA, maxLeftB);
+                }
+            } else if (maxLeftA > minRightB) {
+                right = partitionA - 1;
+            } else {
+                left = partitionA + 1;
+            }
+        }
+
+        return 0.0;
+    }
+};
