ccd9ad770d0d59ae4d5e9d2d1bede34ad438a74b
kartik <kartikswaroop.007@gmail.com>
2024-10-14 23:15:52 +0530

    modified:   Coding Contests/LeetCode Weekly Contest 419/KthLargestPerfectSubtreeSizeInBinaryTree.cpp

--- Coding Contests/LeetCode Weekly Contest 419/KthLargestPerfectSubtreeSizeInBinaryTree.cpp
+++ Coding Contests/LeetCode Weekly Contest 419/KthLargestPerfectSubtreeSizeInBinaryTree.cpp
@@ -13,7 +13,27 @@ struct TreeNode {
 
 class Solution {
 public:
+    int dfs_khLargestPerfectSubtree(TreeNode* root, vector<int>& values) {
+        if (!root)
+            return 0;
+
+        int right = dfs_khLargestPerfectSubtree(root->right, values);
+        int left = dfs_khLargestPerfectSubtree(root->left, values);
+
+        int count_nodes = -1;
+        if (right == left) {
+            count_nodes = (left + right) + 1;
+            values.push_back(count_nodes);
+        }
+
+        return count_nodes;
+    }
+
     int kthLargestPerfectSubtree(TreeNode* root, int k) {
-        
+        vector<int> values;
+
+        int val = dfs_khLargestPerfectSubtree(root, values);
+        sort(values.begin(), values.end(), greater<int>());
+        return k > values.size() ? -1 : values[k - 1];
     }
 };
