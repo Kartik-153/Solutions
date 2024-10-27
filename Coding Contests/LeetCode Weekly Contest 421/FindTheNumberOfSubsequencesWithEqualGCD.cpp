f8547a7bb5bd40e11a61e438a64c007d2de6ea7a
kartik <kartikswaroop.007@gmail.com>
2024-10-27 15:27:26 +0530

    modified:   Coding Contests/LeetCode Weekly Contest 421/FindTheNumberOfSubsequencesWithEqualGCD.cpp

--- Coding Contests/LeetCode Weekly Contest 421/FindTheNumberOfSubsequencesWithEqualGCD.cpp
+++ Coding Contests/LeetCode Weekly Contest 421/FindTheNumberOfSubsequencesWithEqualGCD.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution { // Wrong Answer
+class Solution_wrong { // Wrong Answer
 public:
 const int MOD = 1e9 + 7;
     int subsequencePairCount(vector<int>& nums) {
@@ -48,5 +48,31 @@ const int MOD = 1e9 + 7;
             exp /= 2;
         }
         return result;
+    }
+};
+
+class Solution {
+    const int mod = 1e9 + 7;
+    int dp[200][201][201];
+public:
+    int solve(int i, vector<int>& nums, int first, int second) {
+        if(i == nums.size()) {
+            return (first && second) && (first == second);
+        }
+
+        if(dp[i][first][second] != -1) return dp[i][first][second];
+
+        int skip = solve(i+1, nums, first, second);
+
+        int take1 = solve(i+1, nums, __gcd(first, nums[i]), second);
+
+        int take2 = solve(i+1, nums, first, __gcd(second, nums[i]));
+
+        return dp[i][first][second] = (0LL + skip + take1 + take2) % mod;
+    }
+
+    int subsequencePairCount(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return solve(0, nums, 0, 0);
     }
 };
