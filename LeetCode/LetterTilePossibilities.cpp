5962eb0219163ec07704780237a21be0caf9b6fc
kartik <kartikswaroop.007@gmail.com>
2025-02-17 11:40:03 +0530

    Medium

---
+++ LeetCode/LetterTilePossibilities.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        unordered_set<string> seq;
+        vector<bool> used(tiles.size(), false);
+        generateSeq(tiles, "", used, seq);
+        return seq.size() - 1;
+    }
+
+private:
+    void generateSeq(string &tiles, string curr, vector<bool> &used,
+                     unordered_set<string> &seq) {
+        
+                        seq.insert(curr);
+        for (int pos = 0; pos < tiles.size(); pos++) {
+            if (!used[pos]) {
+                used[pos] = true;
+                generateSeq(tiles, curr + tiles[pos], used, seq);
+                used[pos] = false;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution S;
+    cout << S.numTilePossibilities("90");
+    return 0;
+}
