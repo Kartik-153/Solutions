3bc7cbd7abe7397a58a5b563e46f5c439371e3d6
kartik-153 <kartikswaroop.007@gmail.com>
2025-02-19 09:56:58 +0530

    Medium

--- LeetCode/ThekthLexicographicalStringOfAllHappyStringsOfLengthn.cpp
+++ LeetCode/ThekthLexicographicalStringOfAllHappyStringsOfLengthn.cpp
@@ -29,3 +29,41 @@ private:
     }
 };
 
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        int total = 3 * (1 << (n - 1));
+        if(k > total) return "";
+        string res(n, 'a');
+        unordered_map<char, char> nextSmall = {
+            {'a', 'b'}, {'b', 'a'}, {'c', 'a'}};
+        unordered_map<char, char> nextGreat = {
+            {'a', 'c'}, {'b', 'c'}, {'c', 'b'}};
+        
+        int startA = 1;
+        int startB = startA + (1 << (n-1));
+        int startC = startB + (1 << (n-1));
+
+        if(k < startB) {
+            res[0] = 'a';
+            k -= startA;
+        } else if(k < startC) {
+            res[0] = 'b';
+            k -= startB;
+        } else {
+            res[0] = 'c';
+            k -= startC;
+        }
+
+        for(int i = 1; i < n; i++) {
+            int mid = (1 << (n-i-1));
+            if(k < mid) {
+                res[i] = nextSmall[res[i-1]];
+            } else {
+                res[i] = nextGreat[res[i-1]];
+                k -= mid;
+            }
+        }
+        return res;
+    }
+};
