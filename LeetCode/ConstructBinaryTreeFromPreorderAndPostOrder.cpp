6389bcb846321137717739e236315f45b84447fa
kartik-153 <kartikswaroop.007@gmail.com>
2025-02-23 12:20:26 +0530

    Medium

---
+++ LeetCode/ConstructBinaryTreeFromPreorderAndPostOrder.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public:
+        TreeNode* constructFromPrePost(vector<int>& preorder,
+                                       vector<int>& postorder) {
+            int numOfNodes = preorder.size();
+            return constructTree(0, numOfNodes - 1, 0, preorder, postorder);
+        }
+    
+    private:
+        // Helper function to construct the tree recursively
+        TreeNode* constructTree(int preStart, int preEnd, int postStart,
+                                vector<int>& preorder, vector<int>& postorder) {
+            // Base case: If there are no nodes to process, return NULL
+            if (preStart > preEnd)
+                return NULL;
+    
+            // Base case: If only one node is left, return that node
+            if (preStart == preEnd) {
+                return new TreeNode(preorder[preStart]);
+            }
+    
+            // The left child root in preorder traversal (next element after root)
+            int leftRoot = preorder[preStart + 1];
+    
+            // Calculate the number of nodes in the left subtree by searching in
+            // postorder
+            int numOfNodesInLeft = 1;
+            while (postorder[postStart + numOfNodesInLeft - 1] != leftRoot) {
+                numOfNodesInLeft++;
+            }
+    
+            TreeNode* root = new TreeNode(preorder[preStart]);
+    
+            // Recursively construct the left subtree
+            root->left = constructTree(preStart + 1, preStart + numOfNodesInLeft,
+                                       postStart, preorder, postorder);
+    
+            // Recursively construct the right
+            root->right =
+                constructTree(preStart + numOfNodesInLeft + 1, preEnd,
+                              postStart + numOfNodesInLeft, preorder, postorder);
+    
+            return root;
+        }
+    };
